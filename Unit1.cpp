49ad3fd3d312da5934d37d08d39c7dd327a6b003
AlinaMasetich <alina-masetich@mail.ru>
2017-05-16 23:16:14 +0300

    Add files via upload

---
+++ Unit1.cpp
@@ -0,0 +1,106 @@
+﻿//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+#include "Unit2.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+A obj;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+Memo1->Clear();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender) //Создать
+{
+if(root != NULL) obj.Del_Tree(root);
+	  root = obj.List (StrToInt(Edit1->Text));
+Button3Click(Sender);
+Edit1->Clear();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button3Click(TObject *Sender) //Просмотреть
+{
+Memo1->Clear();
+if( root == NULL ){
+ShowMessage(" Create TREE !");
+}else {
+		  Memo1->Lines->Add("---------- View -----------");
+		  obj.View_Tree(root, 0, Memo1);
+	  }
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button2Click(TObject *Sender) //Добавить
+{
+if(root == NULL) root = obj.List (StrToInt(Edit1->Text));
+else obj.Add_List(root, StrToInt(Edit1->Text));
+Button3Click(Sender);
+Edit1->Clear();
+}
+//---------------------------------------------------------------------------
+
+
+void __fastcall TForm1::Button4Click(TObject *Sender) //Удалить узел
+{
+int Del_Node = StrToInt(Form1->Edit1->Text);
+root = obj.Del_Info(root, Del_Node);
+Button3Click(Sender);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button5Click(TObject *Sender) //Минимальный элемент
+{
+Memo1->Clear();
+Tree *Min_Node = obj.Min_Key(root);
+Memo1->Lines->Add(Min_Node->info);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button6Click(TObject *Sender) //Максимальный элемент
+{
+Memo1->Clear();
+Tree *Max_Node = obj.Max_Key(root);
+Memo1->Lines->Add(Max_Node->info);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button7Click(TObject *Sender) //Кол-во листьев
+{
+Memo1->Clear();
+Memo1->Lines->Add(obj.List_Count(root));
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button8Click(TObject *Sender) //Удалить дерево
+{
+obj.Del_Tree(root);
+Memo1->Clear();
+Edit1->Clear();
+ShowMessage(" Tree Delete!");
+root = NULL;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button9Click(TObject *Sender) //Печать по возрастанию
+{
+Memo1->Clear();
+obj.Left_Order(root, Memo1);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button10Click(TObject *Sender) //Печать по убыванию
+{
+Memo1->Clear();
+obj.Right_Order(root, Memo1);
+}
+//---------------------------------------------------------------------------
+
+
---
+++ Unit2.h
@@ -0,0 +1,190 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef Unit2H
+#define Unit2H
+#include <vcl.h>
+#include <iostream.h>
+#include <string.h>
+#include "Unit1.h"
+
+//---------------------------------------------------------------------------
+#endif
+
+struct Tree{
+	int info;
+	Tree *left, *right;
+}*root;
+
+class A
+{
+public:
+void Add_List(Tree*, int);
+void View_Tree (Tree*, int, TMemo*);
+Tree* Del_Info(Tree*, int);
+void Del_Tree(Tree*);
+Tree* List(int);
+Tree* Min_Key(Tree*);
+Tree* Max_Key(Tree*);
+int List_Count(Tree*);
+void Left_Order(Tree*, TMemo*);
+void Right_Order(Tree*, TMemo*);
+};
+
+Tree* A::List(int inf)//Формирование дерева поиска
+{
+Tree *Create_Tree = new Tree;
+Create_Tree -> info = inf;
+Create_Tree -> left = Create_Tree -> right = NULL;
+return Create_Tree;
+}
+
+void A::Add_List(Tree *root, int key)//Добавление узла
+{
+Tree *prev, *node;
+ bool find = true;
+		node = root;
+		while ( node && find){
+			prev = node;
+			if( key == node->info){
+				find = false;
+				ShowMessage("Dublucate Key!");
+			}else {
+				if ( key < node -> info ){
+					node = node -> left;
+				}else {
+					node = node -> right;
+				}
+			}
+		}
+
+		if (find){
+		node = List(key);
+		if ( key < prev -> info ){
+			prev -> left = node;
+		}else {
+			prev -> right = node;
+		}
+		}
+}
+
+void A::View_Tree(Tree *print, int level, TMemo *Memo1 )//Вывод дерева на экран
+{
+String str_level;
+	if ( print ){
+		View_Tree (print -> right , level+1, Memo1);
+		for ( int i=0; i<level; i++) str_level = str_level + "    ";
+				Memo1->Lines->Add(str_level + IntToStr(print->info));
+		View_Tree(print -> left , level+1, Memo1);
+	}
+}
+
+void A::Del_Tree(Tree *Del)//Очистить память(удалить дерево целиком)
+{
+	if ( Del != NULL){
+		Del_Tree ( Del -> left);
+		Del_Tree ( Del -> right);
+		delete Del;
+	}
+}
+
+Tree* A::Del_Info(Tree *root, int key)//Удаление заданного узла
+{
+Tree *Del, *Prev_Del, *Node, *Prev_Node;
+// Del, Prev_Del – удаляемый узел и его предыдущий (предок);
+// Node, Prev_Node – элемент, на который заменяется удаленный, и его предок;
+Del = root;
+Prev_Del = NULL;
+	while (Del != NULL && Del -> info != key)
+	{
+		Prev_Del = Del;
+		if (Del->info > key){
+			Del = Del->left;
+		}else {
+			Del = Del->right;
+		}
+	}
+
+	if (Del == NULL){
+		ShowMessage ( "NOT Key!");
+			return root;
+	}
+
+	if (Del -> right == NULL){
+	   Node = Del->left;//Поиск элемента Node для замены
+	}else
+		if (Del -> left == NULL){
+			Node = Del->right;
+		}else {
+			Prev_Node = Del;//Ищем самый правый узел в левом поддереве
+			Node = Del->left;
+			while (Node->right != NULL){
+				Prev_Node = Node;
+				Node = Node->right;
+			}
+
+			if( Prev_Node == Del){
+				Node->right = Del->right;// Нашли элемент для замены Node и его предка Prev_Node
+			}else {
+				Node->right = Del->right;
+				Prev_Node->right = Node->left;
+				Node->left = Prev_Node;
+			}
+		}
+	if (Del== root){
+	   root = Node;
+	}else
+		if (Del->info < Prev_Del->info){//Поддерево R присоединяем к предку удаляемого узла
+			Prev_Del->left = Node;
+		   }else {
+				Prev_Del->right = Node;
+		   }
+	delete Del;
+	return root;
+}
+
+Tree* A::Min_Key(Tree *min)//Находим минимальный узел
+{
+while (min->left != NULL)
+	min = min->left;
+ return min;
+}
+
+Tree* A::Max_Key(Tree *max)//Находим максимальный узел
+{
+while (max->right != NULL)
+	max = max->right;
+ return max;
+}
+
+int A::List_Count(Tree* root)//Количество листьев в дереве
+{
+ if (!root)
+  return 0;
+ else if (!root->left && !root->right)
+  return 1;
+ return List_Count(root->left) + List_Count(root->right);
+}
+
+void A::Left_Order(Tree* root, TMemo *Memo1)//Вывести по возрастанию
+{
+if (root->left)
+  Left_Order(root->left, Memo1);
+  Memo1->Lines->Add(root->info);
+if (root->right)
+   Left_Order(root->right, Memo1);
+}
+
+void A::Right_Order(Tree* root, TMemo *Memo1)//Вывести по убыванию
+{
+if (root->right)
+  Right_Order(root->right, Memo1);
+  Memo1->Lines->Add(root->info);
+if (root->left)
+   Right_Order(root->left, Memo1);
+}
+
+
+
+
+
+
